b9a0c7445f20f9396bf2db059bf0f5015ea41672
aivandag <aivan.dev7@gmail.com>
2025-04-08 01:30:48 +0000

    excusegen1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+  let allIcon = ["♦", "♥", "♠", "♣"]
+  let iconTop = document.querySelector("#top")
+  iconTop.innerHTML = allIcon[Math.floor(Math.random()*allIcon.length)]
+
   console.log("Hello Rigo from the console!");
 };
