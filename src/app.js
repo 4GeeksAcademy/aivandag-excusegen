8f8b03a7fbc22801aa53b7c19293efed80e60401
aivandag <aivan.dev7@gmail.com>
2025-04-09 00:49:55 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -7,9 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   //write your code here
-  let allIcon = ["♦", "♥", "♠", "♣"]
-  let iconTop = document.querySelector("#top")
-  iconTop.innerHTML = allIcon[Math.floor(Math.random()*allIcon.length)]
+  let numbers = [ "A", "2","3","4","5","6","7","8","9","10","J","Q","K"]
 
-  console.log("Hello Rigo from the console!");
+  let allIcon = ["♦", "♥", "♠", "♣"]
+
+  generateRandomCard(numbers, allIcon) 
+  
+
+ 
 };
+
+function generateRandomCard(numberArray, iconArray) {
+  let iconTop = document.getElementById("top")
+  let numberCenter = document.getElementById("numbers")
+  let iconBottom = document.getElementById("bottom")
+
+  let randomNumber = numberArray[Math.floor(Math.random()*numberArray.length)]
+  let randomIcon = iconArray[Math.floor(Math.random()*iconArray.length)]
+
+  if (randomIcon == "♦" || randomIcon == "♥") {
+    iconTop.style.color = "red"
+    numberCenter.style.color = "red"
+    iconBottom.style.color = "red"
+  } else {
+    iconTop.style.color = "black"
+    numberCenter.style.color = "black"
+    iconBottom.style.color = "black"
+  }
+
+  iconTop.innerHTML = randomIcon
+  numberCenter.innerHTML = randomNumber
+  iconBottom.innerHTML = randomIcon
+
+}
+
+
+
+// generateRandomCard(otherNumbers)
